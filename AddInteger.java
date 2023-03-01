65090a321581b541e9c5fa621ae9e1dd811f6388
divyaa-007 github <divyakanchap2121@gmail.com>
2023-03-01 13:12:57 +0530

    creating java connection

---
+++ AddInteger.java
@@ -0,0 +1,15 @@
+class AddInteger{
+
+public static void main(String[] args){
+
+System.out.println("Enter two no.s");
+int first = 10;
+int second = 20;
+
+System.out.println(first + "" +second);
+
+//add two numbers
+int sum = first + second;
+System.out.println("the sum is:" +sum);
+}
+}
